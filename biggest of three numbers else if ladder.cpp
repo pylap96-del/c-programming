df1e7f23978788f612cb568231141670570260db
pylap96-del <pylap96@gmail.com>
2025-11-02 15:29:28 +0530

    Add files via upload

---
+++ biggest of three numbers else if ladder.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter a&b&c values");
+	if(a>b && a>c)
+	printf("a is big");
+	else if(b>c)
+	printf("b is big");
+	else
+	printf("c is big");
+	return 0;
+}
