ce92c5381112f3d1a25b2a65a2716363b1493c44
pylap96-del <pylap96@gmail.com>
2025-11-12 15:35:01 +0530

    Add files via upload

---
+++ primary data types.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x=10;
+	char ch='a';
+	float pi=3.14;
+	double p=12345.0679;
+	long int q=6553523;
+	printf("x=%d",x);
+	printf("ch=%c",ch);
+	printf("pi=%f",pi);
+	printf("p=%lf",p);
+	printf("q=%ld",q);
+	return 0;
+}
